74a9f717d51e6ed3462725935906d3b4b7adb5ec
Aureliano Martini <aure.martini@gmail.com>
2023-09-16 13:51:44 +0200

    Minor fix

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_totem';
-$plugin->version = 2023090501;
+$plugin->version = 2023091600;
 
 $plugin->requires = 2014051200; // Moodle 2.7.0 is required.
-$plugin->supported = [37, 39, 40, 41]; // Moodle 3.7.x, 3.8.x, 3.9.x, 4.0.x and 4.1.x are supported.
+$plugin->supported = [37, 41]; // Moodle 3.7.x, 3.8.x, 3.9.x, 4.0.x and 4.1.x are supported.
 //$plugin->incompatible = 36; // Moodle 3.6.x and later are incompatible.
 
 $plugin->maturity = MATURITY_ALPHA; //Supported value is any of the predefined constants MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC or MATURITY_STABLE.
